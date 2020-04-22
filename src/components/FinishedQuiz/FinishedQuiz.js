ea3b759a0d5a40405314730a58a0d694fd874ee2
NimfaMargo <Nikiforova.Rita@gmail.com>
2020-04-22 23:59:23 +0300

    show results

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -2,21 +2,27 @@ import React from 'react';
 import classes from './FinishedQuiz.module.css';
 
 const FinishedList = (props) => {
+    const { quiz, results } = props;
+    console.log(quiz, results);
+    
     return(
         <div className={classes.FinishedQuiz}>
            <ul>
-               <li>
-                   <strong>1. </strong>
-                   gg
-                   <i className={'fa fa-times ' + classes.error} />
-               </li>
-                <li>
-                    <strong>2. </strong>
-                    bb  
-                    <i className={'fa fa-check ' + classes.success} />
-                </li>
+               {quiz.map((quizItem, index) => {
+                   const cls = [
+                        "fa",
+                        results[quizItem.id] === "error" ? "fa-times" : "fa-check",
+                        classes[results[quizItem.id]],
+                   ];
+                   return (
+                     <li key={index}>
+                       <strong>{index + 1}</strong>&nbsp;
+                       {quizItem}
+                       <i className={cls.join(' ')} />
+                     </li>
+                   );
+               })}  
            </ul>
-
            <p>Правильно 4 из 10</p>
            <div>
                <button>Повторить</button>
