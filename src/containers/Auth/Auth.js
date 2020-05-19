6f9676a37e65421235ba230e35a0e0c9e7d3c906
NimfaMargo <Nikiforova.Rita@gmail.com>
2020-05-20 00:18:46 +0400

    add auth form

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import classes from './Auth.module.css';
+import Button from '../../components/UI/Button/Button';
 
 class Auth extends Component {
-
+	registerHandler = () => {};
+	loginHandler = () => {};
+	submitHandler = (e) => {
+		e.preventDafault();
+	}
 	render() { 
 		return (
-			<div>
-				<h1>Auth</h1>
+			<div className={classes.Auth}>
+				<div>
+					<h1>Авторизация</h1>
+					<form className={classes.AuthForm} onSubmit={this.submitHandler}>
+						<input type='text'/>
+						<input type='text'/>
+						<Button 
+							type='success'
+							onClick={this.loginHandler}
+						>
+							Войти
+						</Button>
+						<Button
+							type='primary'
+							onClick={this.registerHandler}
+						>
+							Зарегестрироваться
+						</Button>
+					</form>
+				</div>
 			</div> 
 		);
 	}
