19e5df3725c528306bb3c5042a9ec6c15699c1a9
NimfaMargo <Nikiforova.Rita@gmail.com>
2020-10-22 18:48:20 +0300

    refactor(ui): use button as а functional component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,25 +1,19 @@
 import React from 'react';
+import cn from 'classnames';
 import classes from './Button.module.css';
 
-const Button = (props) => {
-  const {
-    disabled, children, type, onClick,
-  } = props;
-
-  const cls = [
-    classes.Button,
-    classes[type],
-  ];
-
-  return (
-    <div
-      onClick={onClick}
-      disabled={disabled}
-      className={cls.join(' ')}
-    >
+export const Button = ({
+ disabled, children, type, onClick,
+}) => (
+	<div
+		role="button"
+		aria-hidden="true"
+		onClick={onClick}
+		disabled={disabled}
+		className={cn({ [classes.Button]: true, [classes[type]]: true })}
+	>
       {children}
-    </div>
-  );
-};
+	</div>
+);
 
 export default Button;
