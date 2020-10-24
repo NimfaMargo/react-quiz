43bdbb72df4043e20398dcc4fb0cc346deb9e1ca
NimfaMargo <Nikiforova.Rita@gmail.com>
2020-10-24 23:43:11 +0300

    style(FinishedQuiz): fix linter issues, indents

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,41 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './FinishedQuiz.module.css';
-import Button from '../UI/Button/Button';
+import { Button } from '../UI/Button/Button';
 
-const FinishedList = (props) => {
-  const { quiz, results, onRetry } = props;
+const FinishedList = ({ quiz, results, onRetry }) => {
+	const numberOfRightResults = Object.values(results).reduce((acc, value) => (value === 'success' ? acc + 1 : acc), 0);
 
-  const numberOfRightResults = Object.values(results).reduce((acc, value) => (value === 'success' ? acc + 1 : acc), 0);
-
-  return (
-    <div className={classes.FinishedQuiz}>
-      <ul>
-        {quiz.map((quizItem, index) => {
-          const cls = [
-            'fa',
-            results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
-            classes[results[quizItem.id]],
-          ];
-          return (
-            <li key={index}>
-              <strong>{index + 1}</strong>
-&nbsp;
-              {quizItem.question}
-              <i className={cls.join(' ')} />
-            </li>
-          );
-        })}
-      </ul>
-      <p>{`Правильно ${numberOfRightResults} из ${quiz.length}`}</p>
-      <div>
-        <Button type="primary" onClick={onRetry}>Повторить</Button>
-        <Link to="/">
-          <Button type="success">Перейти в список тестов</Button>
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className={classes.FinishedQuiz}>
+			<ul>
+				{quiz.map((quizItem, index) => {
+					const cls = [
+						'fa',
+						results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
+						classes[results[quizItem.id]],
+					];
+					return (
+						<li key={quiz.id}>
+							<strong>{index + 1}</strong>
+								&nbsp;
+							{quizItem.question}
+							<i className={cls.join(' ')} />
+						</li>
+					);
+				})}
+			</ul>
+			<p>{`Правильно ${numberOfRightResults} из ${quiz.length}`}</p>
+			<div>
+				<Button type="primary" onClick={onRetry}>Повторить</Button>
+				<Link to="/">
+				<Button type="success">Перейти в список тестов</Button>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default FinishedList;
